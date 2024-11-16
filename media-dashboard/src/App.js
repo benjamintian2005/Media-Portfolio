c5e80696490a94d98feb4f36d50c0aecd027c9bd
Benjamin <ben@art.com>
2024-11-15 19:33:11 -0500

    Added images and data functionality

---
+++ media-dashboard/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+import MediaDashboard from './components/MediaDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <MediaDashboard />
+    </div>
+  );
+}
+
+export default App;
